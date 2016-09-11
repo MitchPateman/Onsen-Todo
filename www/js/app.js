b9908ce8cf384ebf1098d4c58dee76be3ed0d5cf
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-11 18:42:32 -0400

    Added code for fixtures in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7,4 +7,14 @@ document.addEventListener('init', function(event) {
     myApp.controllers[page.id](page);
   }
 
+  // Fill the lists with initial data when the pages we need are ready.
+    // This only happens once at the beginning of the app.
+    if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
+      if (document.querySelector('#menuPage') && document.querySelector('#pendingTasksPage')) {
+        myApp.services.fixtures.forEach(function(data) {
+          myApp.services.tasks.create(data);
+        });
+      }
+    }
+
 });
