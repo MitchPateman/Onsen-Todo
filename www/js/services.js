8ecd1da52fd702e9fa4271fee4b0f4e6a05dd9e7
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-24 16:43:27 -0400

    DELETING ALL FIREBASE REFS, BUT THE ID IS NOW CONSOLE LOGGING ON CREATE

--- www/js/services.js
+++ www/js/services.js
@@ -78,7 +78,7 @@ myApp.services = {
       myApp.services.animators.remove(taskItem, function() {
         // Remove the item before updating the categories.
         taskItem.remove();
-        firebase.database().ref(taskRef.key).remove()
+        firebase.database().ref(taskRef).remove()
 
         // Check if the category has no items and remove it in that case.
         myApp.services.categories.updateRemove(taskItem.data.category);
