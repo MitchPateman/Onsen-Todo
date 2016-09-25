0dd8f056fa3c2d508c95d0f770ef8eaa9255ae51
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-25 00:37:35 -0400

    try to add a counter to 1 to stop infinite create loop

--- www/js/app.js
+++ www/js/app.js
@@ -11,13 +11,15 @@ document.addEventListener('init', function(event) {
     //This only happens once at the beginning of the app.
     if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
       if (document.querySelector('#menuPage') && document.querySelector('#pendingTasksPage')) {
+
         // Attach an asynchronous callback to read the data at our posts reference
         db.on("child_added", function(snapshot) {
           var fillData = snapshot.val();
           console.log(fillData);
-
-            if (fillData) {
+          var x = 0;
+            if (x < 1) {
               myApp.services.tasks.create(fillData);
+              x++;
             }
         }, function (errorObject) {
           console.log("The read failed: " + errorObject.code);
