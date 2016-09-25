a7a92ff3326fc91cf2b261abab3c147d755d1fc5
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-24 23:24:24 -0400

    testing fill data

--- www/js/app.js
+++ www/js/app.js
@@ -7,16 +7,20 @@ document.addEventListener('init', function(event) {
     myApp.controllers[page.id](page);
   }
 
-  // Fill the lists with initial data from firebase when the pages we need are ready.
+  // Fill the lists with initial data when the pages we need are ready.
     //This only happens once at the beginning of the app.
     if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
       if (document.querySelector('#menuPage') && document.querySelector('#pendingTasksPage')) {
         // Attach an asynchronous callback to read the data at our posts reference
-          db.on("value", function(snapshot) {
-            console.log(snapshot.val());
-          }, function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-          });
+        db.on("value", function(snapshot) {
+          console.log(snapshot.val());
+          var fillData = snapshot.val();
+        }, function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
+        fillData.forEach(function(data) {
+          myApp.services.tasks.create(data);
+        });
       }
     }
 
