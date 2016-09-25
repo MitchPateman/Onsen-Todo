11ecfd327963d6abd73c50378fb243b7e9eca22c
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-25 01:13:43 -0400

    INFINITE LOOP IN APP.JS, OR IN SERVICES CREATE on DB READ

--- www/js/app.js
+++ www/js/app.js
@@ -17,8 +17,13 @@ document.addEventListener('init', function(event) {
           var fillData = snapshot.val();
           console.log(fillData.data);
           var data = fillData.data;
+
+
+//THIS IS BROKEN???? INFINITE LOOP IN APP.JS, OR IN SERVICES CREATE on DB READ//
           //POPULATE THE LIST
           myApp.services.tasks.create(data);
+
+
           //firebase read error msg
         }, function (errorObject) {
           console.log("The read failed: " + errorObject.code);
