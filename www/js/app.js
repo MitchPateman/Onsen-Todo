1f1dce9481726b550ab94883e708dedba8565cc5
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-25 00:54:39 -0400

    dont fillData yet, test in console

--- www/js/app.js
+++ www/js/app.js
@@ -6,20 +6,20 @@ document.addEventListener('init', function(event) {
   if (myApp.controllers.hasOwnProperty(page.id)) {
     myApp.controllers[page.id](page);
   }
-  var added;
+
   // Fill the lists with initial data when the pages we need are ready.
     //This only happens once at the beginning of the app.
     if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
       if (document.querySelector('#menuPage') && document.querySelector('#pendingTasksPage')) {
-          // Attach an asynchronous callback to read the data at our posts reference
-          db.on("child_added", function(snapshot) {
-            var fillData = snapshot.val();
-            console.log(fillData);
-                //myApp.services.tasks.create(fillData.data);
-          }, function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-          });
-        }
+
+        // Attach an asynchronous callback to read the data at our posts reference
+        db.on("child_added", function(snapshot) {
+          var fillData = snapshot.val();
+          console.log(fillData);
+              //myApp.services.tasks.create(fillData);
+        }, function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
       }
     }
 
