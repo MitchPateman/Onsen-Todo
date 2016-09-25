1be95f4ffcddf96359aea33e9d1542019513e588
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-24 22:04:08 -0400

    go back

--- www/js/app.js
+++ www/js/app.js
@@ -9,12 +9,12 @@ document.addEventListener('init', function(event) {
 
   // Fill the lists with initial data when the pages we need are ready.
     //This only happens once at the beginning of the app.
-    if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
-      if (document.querySelector('#menuPage') && document.querySelector('#pendingTasksPage')) {
-        myApp.services.fixtures.forEach(function(data) {
-          myApp.services.tasks.create(data);
-        });
-      }
-    }
+    // if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
+    //   if (document.querySelector('#menuPage') && document.querySelector('#pendingTasksPage')) {
+    //     myApp.services.fixtures.forEach(function(data) {
+    //       myApp.services.tasks.create(data);
+    //     });
+    //   }
+    // }
 
 });
