88ba09177ffd7d0cbfc29ad1099e961a2ad56e9c
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-25 00:41:49 -0400

    added is false or true

--- www/js/app.js
+++ www/js/app.js
@@ -6,23 +6,24 @@ document.addEventListener('init', function(event) {
   if (myApp.controllers.hasOwnProperty(page.id)) {
     myApp.controllers[page.id](page);
   }
-
+  var added = false;
   // Fill the lists with initial data when the pages we need are ready.
     //This only happens once at the beginning of the app.
     if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
       if (document.querySelector('#menuPage') && document.querySelector('#pendingTasksPage')) {
-
-        // Attach an asynchronous callback to read the data at our posts reference
-        db.on("child_added", function(snapshot) {
-          var fillData = snapshot.val();
-          console.log(fillData);
-            if (var x < 1) {
-              myApp.services.tasks.create(fillData);
-              x++;
-            }
-        }, function (errorObject) {
-          console.log("The read failed: " + errorObject.code);
-        });
+        if added == false {
+          // Attach an asynchronous callback to read the data at our posts reference
+          db.on("child_added", function(snapshot) {
+            var fillData = snapshot.val();
+            console.log(fillData);
+              if (x < 1) {
+                myApp.services.tasks.create(fillData);
+                added = true;
+              }
+          }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+          });
+        }
       }
     }
 
