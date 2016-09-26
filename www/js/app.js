f19ab3d8ba0e9f8c67947e2fa047931bb079c02b
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-26 02:09:43 -0400

    moving child removed into init func

--- www/js/app.js
+++ www/js/app.js
@@ -22,18 +22,18 @@ document.addEventListener('init', function(event) {
           }, function (errorObject) {
             console.log("The read failed: " + errorObject.code);
           });
-      }
-    }
 
-    // REMOVE FROM THE LIST IF ITS DELETED FROM FIREBASE
-    // Get the data on a post that has been removed
-    db.on("child_removed", function(snapshot) {
-      var deletedPost = snapshot.val();
-      console.log("The task titled '" + deletedPost.data.title + "' has been deleted from firebase, and now your app!");
-      console.log(deletedPost.data);
-        var taskItem = deletedPost.data
-      //Remove the task item that matches this data
-      myApp.services.tasks.remove(taskItem);
-    });
-    
+          // REMOVE FROM THE LIST IF ITS DELETED FROM FIREBASE
+          // Get the data on a post that has been removed
+          db.on("child_removed", function(snapshot) {
+            var deletedPost = snapshot.val();
+            console.log("The task titled '" + deletedPost.data.title + "' has been deleted from firebase, and now your app!");
+            console.log(deletedPost.data);
+              var taskItem = deletedPost.data
+            //Remove the task item that matches this data
+            myApp.services.tasks.remove(taskItem);
+          });
+      };
+    };
+
 });
