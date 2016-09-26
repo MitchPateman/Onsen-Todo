01dcaaa43f0223333d0c81d693d3945e0da91c75
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-26 14:44:35 -0400

    for loop added

--- www/js/app.js
+++ www/js/app.js
@@ -33,10 +33,11 @@ document.addEventListener('init', function(event) {
             //Get the matching task list Item to the firebase task
             var pendingList = document.querySelector('#pending-list');
             var listItemCenter = pendingList.querySelectorAll('.center.list__item__center')
-              console.log(pendingList);
-              console.log(listItemCenter.innerHTML);
+            console.log(pendingList);
 
-
+            for (var i = 0; i < listItemCenter.length; i++) {
+              console.log(listItemCenter[i]);
+            }
 
 
             var taskItem = deletedTask.data;
