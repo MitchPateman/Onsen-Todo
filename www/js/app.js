685ad3d4879a864ef5ce3d5bd21d051ac335aab1
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-26 17:38:53 -0400

    wroking????

--- www/js/app.js
+++ www/js/app.js
@@ -20,12 +20,14 @@ document.addEventListener('init', function(event) {
             //POPULATE THE LIST
             var pendingList = document.querySelector('#pending-list');
             var listItemCenter = pendingList.querySelectorAll('.center.list__item__center');
-              if (listItemCenter[0] && data.title !== listItemCenter[listItemCenter.length-1].innerHTML) {
-                myApp.services.tasks.create(data);
-              }
-              else if (!listItemCenter[0]) {
+            if (listItemCenter[0]){
+              if (data.title !== listItemCenter[listItemCenter.length-1].innerHTML) {
                 myApp.services.tasks.create(data);
               };
+            };
+            // else {
+            //   myApp.services.tasks.create(data);
+            // }
             //firebase read error msg
           }, function (errorObject) {
             console.log("The read failed: " + errorObject.code);
