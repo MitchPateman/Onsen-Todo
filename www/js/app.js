f5e9de81427c1b7d83580c68ef0f11ea33baecff
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-26 17:58:01 -0400

    might have it

--- www/js/app.js
+++ www/js/app.js
@@ -18,16 +18,7 @@ document.addEventListener('init', function(event) {
               var data = fillData.data;
               console.log(data);
             //POPULATE THE LIST
-            var pendingList = document.querySelector('#pending-list');
-            var listItemCenter = pendingList.querySelectorAll('.center.list__item__center');
-            if (listItemCenter[0]){
-              if (data.title !== listItemCenter[listItemCenter.length].innerHTML) {
                 myApp.services.tasks.create(data);
-              };
-            }
-            else {
-              myApp.services.tasks.create(data);
-            }
             //firebase read error msg
           }, function (errorObject) {
             console.log("The read failed: " + errorObject.code);
