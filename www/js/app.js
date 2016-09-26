bcb41931ab562d72da62bbdf93772f7a9ebd363c
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-26 14:31:25 -0400

    CLOSE

--- www/js/app.js
+++ www/js/app.js
@@ -32,7 +32,7 @@ document.addEventListener('init', function(event) {
 
             //Get the matching task list Item to the firebase task
             var pendingList = document.querySelector('#pending-list');
-            var listItemCenter = pendingList.querySelectorAll('.center.list__item__center.innerText')
+            var listItemCenter = pendingList.querySelectorAll('.center.list__item__center')
               console.log(pendingList);
               console.log(listItemCenter);
 
