a27180dc103dba35dbf27f8c61fa3ffc4a70d6b8
Mitch Pateman <mitch_pateman@hotmail.com>
2016-09-25 23:32:22 -0400

    deletedPost

--- www/js/app.js
+++ www/js/app.js
@@ -17,13 +17,19 @@ document.addEventListener('init', function(event) {
               var data = fillData.data;
               console.log(data);
             //POPULATE THE LIST
-                myApp.services.tasks.create(data);
-
+              myApp.services.tasks.create(data);
             //firebase read error msg
           }, function (errorObject) {
             console.log("The read failed: " + errorObject.code);
           });
 
+          // REMOVE FROM THE LIST IF ITS DELETED FROM FIREBASE
+          // Get the data on a post that has been removed
+          ref.on("child_removed", function(snapshot) {
+            var deletedPost = snapshot.val();
+            console.log("The task titled '" + deletedPost.title + "' has been deleted from firebase, and now your app!");
+            console.log(deletedPost);
+          });
       }
     }
 
